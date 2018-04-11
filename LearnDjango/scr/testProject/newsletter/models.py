0c34884e7e39b6af61164e1ca42e47e023eb40d1
AnandDev006 <anand.dev.006@gmail.com>
2018-04-12 03:01:11 +0530

    12/4/18

--- LearnDjango/scr/testProject/newsletter/models.py
+++ LearnDjango/scr/testProject/newsletter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class SignUp(models.Model):
+    email = models.EmailField()
+    fullName = models.CharField( blank = True, null = True, max_length = 30)
+    timeStamp = models.DateTimeField( auto_now_add = True, auto_now = False)
+    updated = models.DateTimeField( auto_now_add = False, auto_now = True)
+
+    class Meta:
+        verbose_name_plural = "SignUp"
+
+    def __str__(self):
+        return self.fullName
--- LearnDjango/scr/testProject/newsletter/views.py
+++ LearnDjango/scr/testProject/newsletter/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import SignUpForm
 
 # Create your views here.
+
+def index(request):
+    title = "My Title"
+    form = SignUpForm(request.POST or None)
+    if form.is_valid():
+        saved_form = form.save(commit = False)
+        saved_form.save()
+    context = {
+        "title": title,
+        "form": form,
+    }
+    return render( request, 'newsletter/index.html', context)
